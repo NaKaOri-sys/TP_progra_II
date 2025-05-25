c63f5bb1cc883eace70e9ff4d2be20a55743a352
Nahuel Nievas <nahuelnievas30@gmail.com>
2025-05-24 23:03:36 -0300

    Mejoras a nivel general
    feat: se agregan mejoras en Espectaculo. feat: se refactoriza metodo para inicializar sectores, se modulariza para validar los parametros en otro metodo

--- ar/edu/ungs/prog2/ticketek/MiniEstadioCubierto.java
+++ ar/edu/ungs/prog2/ticketek/MiniEstadioCubierto.java
@@ -16,17 +16,42 @@ public class MiniEstadioCubierto extends Sede {
 		this.precioConsumicion = precioConsumicion;
 		inicializarSectores(tipoSectores, capacidadMaxPorSector, asientosPorFila, porcentajeIncrementoSectores);
 	}
-
+	
+	/**
+	 * Función privada que se encarga de inicializar los sectores, en caso
+	 * de que los parametros no tengan un sentido lógico o no correspondan se lanzara IllegalArgumentException
+	 * @param tipoSectores
+	 * @param capacidadMaxPorSector
+	 * @param asientosPorFila
+	 * @param porcentajeIncrementoSectores
+	 * @throws IllegalArgumentException
+	 */
 	private void inicializarSectores(String[] tipoSectores, int[] capacidadMaxPorSector, int asientosPorFila, int[] porcentajeIncrementoSectores) throws Exception {
-		if (tipoSectores.length == 0 || capacidadMaxPorSector.length == 0 || porcentajeIncrementoSectores.length == 0
-				|| capacidadMaxPorSector.length != tipoSectores.length
-				|| porcentajeIncrementoSectores.length != tipoSectores.length) {
-			throw new Exception("Para poder inicializar los sectores, los tipos de sectores deben tener la misma cantidad de elementos que capacidadMaxPorSector y porcentajeIncrementoSectores");
-		}
+		validarParametrosSectores(tipoSectores, capacidadMaxPorSector, porcentajeIncrementoSectores);
 		for (int i = 0; i < tipoSectores.length; i++) {
 			sectoresRegistrados.put(tipoSectores[i], new Sector(tipoSectores[i],
 					asientosPorFila, capacidadMaxPorSector[i],porcentajeIncrementoSectores[i]));			
 		}
+	}
+	
+	/**
+	 * Valida que los arrays de parámetros para la inicialización de sectores sean consistentes.
+	 *
+	 * @param tipoSectores Un array de tipos de sectores.
+	 * @param capacidadMaxPorSector  Un array de capacidades máximas por sector.
+	 * @param porcentajeIncrementoSectores Un array de porcentajes de incremento por sector.
+	 * @throws IllegalArgumentException Si los arrays no tienen la misma longitud o están vacíos.
+	 */
+	private void validarParametrosSectores(String[] tipoSectores, int[] capacidadMaxPorSector, int[] porcentajeIncrementoSectores) throws IllegalArgumentException {
+	    if (tipoSectores == null || capacidadMaxPorSector == null || porcentajeIncrementoSectores == null) {
+	        throw new IllegalArgumentException("Los arrays de tipos de sectores, capacidades o incrementos no pueden ser nulos.");
+	    }
+	    if (tipoSectores.length == 0 || capacidadMaxPorSector.length == 0 || porcentajeIncrementoSectores.length == 0) {
+	        throw new IllegalArgumentException("Los arrays de tipos de sectores, capacidades o incrementos no pueden estar vacíos.");
+	    }
+	    if (capacidadMaxPorSector.length != tipoSectores.length || porcentajeIncrementoSectores.length != tipoSectores.length) {
+	        throw new IllegalArgumentException("Para poder inicializar los sectores, los tipos de sectores deben tener la misma cantidad de elementos que capacidadMaxPorSector y porcentajeIncrementoSectores.");
+	    }
 	}
 
 	@Override
@@ -44,7 +69,13 @@ public class MiniEstadioCubierto extends Sede {
 	public HashMap<String, Sector> obtenerSectores() {
 		return sectoresRegistrados;
 	}
-
+	
+	/**
+	 * Obtiene el sector en base al tipo enviado, en caso de que este no exista en los sectores registrados, lanza Exception
+	 * @param tipo
+	 * @return
+	 * @throws Exception
+	 */
 	public Sector obtenerSector(String tipo) throws Exception {
 		if (!sectoresRegistrados.containsKey(tipo)) {
 			throw new Exception("El sector a buscar no existe.");
