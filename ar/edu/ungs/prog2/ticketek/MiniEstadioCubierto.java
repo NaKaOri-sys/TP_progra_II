b2cd482ac48269e0a4857ce5d985534e6e55ec1e
Nahuel Nievas <nahuelnievas30@gmail.com>
2025-05-26 00:08:55 -0300

    correcciones generales
    fix: correcciones en general.
    fix: modificaciones para tener info de los sectores

--- ar/edu/ungs/prog2/ticketek/MiniEstadioCubierto.java
+++ ar/edu/ungs/prog2/ticketek/MiniEstadioCubierto.java
@@ -1,6 +1,7 @@
 package TP_progra_II.ar.edu.ungs.prog2.ticketek;
 
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class MiniEstadioCubierto extends Sede {
 	HashMap<String, Sector> sectoresRegistrados;
@@ -9,49 +10,61 @@ public class MiniEstadioCubierto extends Sede {
 	private double precioConsumicion;
 
 	public MiniEstadioCubierto(String nombre, String direccion, int capacidadMaxima, double precioConsumicion,
-			String[] tipoSectores, int[] capacidadMaxPorSector, int asientosPorFila, 
+			String[] tipoSectores, int[] capacidadMaxPorSector, int asientosPorFila,
 			int[] porcentajeIncrementoSectores) {
 		super(nombre, direccion, capacidadMaxima);
 		this.sectoresRegistrados = new HashMap<String, Sector>();
 		this.precioConsumicion = precioConsumicion;
 		inicializarSectores(tipoSectores, capacidadMaxPorSector, asientosPorFila, porcentajeIncrementoSectores);
 	}
-	
+
 	/**
-	 * Función privada que se encarga de inicializar los sectores, en caso
-	 * de que los parametros no tengan un sentido lógico o no correspondan se lanzara IllegalArgumentException
+	 * Función privada que se encarga de inicializar los sectores, en caso de que
+	 * los parametros no tengan un sentido lógico o no correspondan se lanzara
+	 * IllegalArgumentException
+	 * 
 	 * @param tipoSectores
 	 * @param capacidadMaxPorSector
 	 * @param asientosPorFila
 	 * @param porcentajeIncrementoSectores
 	 * @throws IllegalArgumentException
 	 */
-	private void inicializarSectores(String[] tipoSectores, int[] capacidadMaxPorSector, int asientosPorFila, int[] porcentajeIncrementoSectores) {
+	private void inicializarSectores(String[] tipoSectores, int[] capacidadMaxPorSector, int asientosPorFila,
+			int[] porcentajeIncrementoSectores) {
 		validarParametrosSectores(tipoSectores, capacidadMaxPorSector, porcentajeIncrementoSectores);
 		for (int i = 0; i < tipoSectores.length; i++) {
-			sectoresRegistrados.put(tipoSectores[i], new Sector(tipoSectores[i],
-					asientosPorFila, capacidadMaxPorSector[i],porcentajeIncrementoSectores[i]));			
+			sectoresRegistrados.put(tipoSectores[i], new Sector(tipoSectores[i], asientosPorFila,
+					capacidadMaxPorSector[i], porcentajeIncrementoSectores[i]));
 		}
 	}
-	
+
 	/**
-	 * Valida que los arrays de parámetros para la inicialización de sectores sean consistentes.
+	 * Valida que los arrays de parámetros para la inicialización de sectores sean
+	 * consistentes.
 	 *
-	 * @param tipoSectores Un array de tipos de sectores.
-	 * @param capacidadMaxPorSector  Un array de capacidades máximas por sector.
-	 * @param porcentajeIncrementoSectores Un array de porcentajes de incremento por sector.
-	 * @throws IllegalArgumentException Si los arrays no tienen la misma longitud o están vacíos.
+	 * @param tipoSectores                 Un array de tipos de sectores.
+	 * @param capacidadMaxPorSector        Un array de capacidades máximas por
+	 *                                     sector.
+	 * @param porcentajeIncrementoSectores Un array de porcentajes de incremento por
+	 *                                     sector.
+	 * @throws IllegalArgumentException Si los arrays no tienen la misma longitud o
+	 *                                  están vacíos.
 	 */
-	private void validarParametrosSectores(String[] tipoSectores, int[] capacidadMaxPorSector, int[] porcentajeIncrementoSectores) {
-	    if (tipoSectores == null || capacidadMaxPorSector == null || porcentajeIncrementoSectores == null) {
-	        throw new IllegalArgumentException("Los arrays de tipos de sectores, capacidades o incrementos no pueden ser nulos.");
-	    }
-	    if (tipoSectores.length == 0 || capacidadMaxPorSector.length == 0 || porcentajeIncrementoSectores.length == 0) {
-	        throw new IllegalArgumentException("Los arrays de tipos de sectores, capacidades o incrementos no pueden estar vacíos.");
-	    }
-	    if (capacidadMaxPorSector.length != tipoSectores.length || porcentajeIncrementoSectores.length != tipoSectores.length) {
-	        throw new IllegalArgumentException("Para poder inicializar los sectores, los tipos de sectores deben tener la misma cantidad de elementos que capacidadMaxPorSector y porcentajeIncrementoSectores.");
-	    }
+	private void validarParametrosSectores(String[] tipoSectores, int[] capacidadMaxPorSector,
+			int[] porcentajeIncrementoSectores) {
+		if (tipoSectores == null || capacidadMaxPorSector == null || porcentajeIncrementoSectores == null) {
+			throw new IllegalArgumentException(
+					"Los arrays de tipos de sectores, capacidades o incrementos no pueden ser nulos.");
+		}
+		if (tipoSectores.length == 0 || capacidadMaxPorSector.length == 0 || porcentajeIncrementoSectores.length == 0) {
+			throw new IllegalArgumentException(
+					"Los arrays de tipos de sectores, capacidades o incrementos no pueden estar vacíos.");
+		}
+		if (capacidadMaxPorSector.length != tipoSectores.length
+				|| porcentajeIncrementoSectores.length != tipoSectores.length) {
+			throw new IllegalArgumentException(
+					"Para poder inicializar los sectores, los tipos de sectores deben tener la misma cantidad de elementos que capacidadMaxPorSector y porcentajeIncrementoSectores.");
+		}
 	}
 
 	@Override
@@ -69,9 +82,11 @@ public class MiniEstadioCubierto extends Sede {
 	public HashMap<String, Sector> obtenerSectores() {
 		return sectoresRegistrados;
 	}
-	
+
 	/**
-	 * Obtiene el sector en base al tipo enviado, en caso de que este no exista en los sectores registrados, lanza Exception
+	 * Obtiene el sector en base al tipo enviado, en caso de que este no exista en
+	 * los sectores registrados, lanza Exception
+	 * 
 	 * @param tipo
 	 * @return
 	 * @throws Exception
@@ -90,5 +105,25 @@ public class MiniEstadioCubierto extends Sede {
 
 	public int obtenerPuestosComidaRapida() {
 		return puestosComidaRapida;
+	}
+
+	@Override
+	public String obtenerInfoSectores(HashMap<String, Integer> entradasVendidasPorSector) {
+		StringBuilder sb = new StringBuilder();
+		boolean firstSector = true;
+		for (Entry<String, Sector> entry : this.sectoresRegistrados.entrySet()) {
+			if (!firstSector) {
+				sb.append(" | ");
+			}
+			String tipoSector = entry.getKey();
+			Sector sector = entry.getValue();
+
+			int entradasVendidas = entradasVendidasPorSector.getOrDefault(tipoSector, 0);
+			int capacidadSector = sector.obtenerCapacidadMaxSector();
+
+			sb.append(tipoSector).append(": ").append(entradasVendidas).append("/").append(capacidadSector);
+			firstSector = false;
+		}
+		return sb.toString();
 	}
 }
